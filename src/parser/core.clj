7a9eaa0a821da84d986bef9df9d5ff696132e044
Luiz Rodrigo de Souza <me@lurodrigo.com>
2020-08-20 09:26:51 -0300

    small simplifications

--- src/parser/core.clj
+++ src/parser/core.clj
@@ -7,16 +7,15 @@
     [rewrite-clj.parser :as parser]))
 
 (defn node->data
-  "Provides an representation of the node that's convenient for diffing."
+  "Provides a representation of the node that's convenient for diffing."
   [node]
-  (let [tag      (node/tag node)
-        children (->> (try
+  (let [children (->> (try
                         (node/children node)
                         (catch Throwable _ []))
                       (map node->data))]
     (if (empty? children)
       (node/string node)
-      (vec (cons tag children)))))
+      (vec (cons (node/tag node) children)))))
 
 (defn data->node
   [data]
@@ -34,31 +33,27 @@
                         :var node/var-node)]
       (constructor (map data->node children)))))
 
+(def as-code (comp node/string data->node))
+
 (defn apply-edit
   [data [location op new-value :as edit]]
   (case op
-    :- (printf "Delete «%s»:\n" (-> (get-in data location)
-                                    ; print*
-                                    data->node
-                                    node/string))
-    :+ (printf "Add «%s»:\n"  (-> new-value data->node node/string))
-    :r (printf "Replace «%s» with «%s»:\n" (-> (get-in data location)
-                                               ; print*
-                                               data->node
-                                               node/string)
-               (-> new-value data->node node/string)))
+    :- (printf "Delete «%s»:\n" (as-code (get-in data location)))
+    :+ (printf "Add «%s»:\n"  (as-code new-value))
+    :r (printf "Replace «%s» with «%s»:\n" (as-code (get-in data location))
+               (as-code new-value)))
   (let [result (e/patch data (edit/edits->script [edit]))]
-    (println (-> result data->node node/string) "\n")
+    (println (as-code result) "\n")
     result))
 
 (defn apply-edits
   [data edits]
-  (printf "Starting with\n%s\n\n" (-> data data->node node/string))
+  (printf "Starting with\n%s\n\n" (as-code data))
   (reduce apply-edit data (edit/get-edits edits)))
 
 (comment
   (def code1 (parser/parse-string "(defn x 1)"))
-  (def code2 (parser/parse-string "(defn y [1])"))
+  (def code2 (parser/parse-string "(defn y [x] (* x 1))"))
   (def data1 (node->data code1))
   (def data2 (node->data code2))
   (def edits (e/diff data1 data2))
